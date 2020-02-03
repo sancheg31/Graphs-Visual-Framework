2ef36943a9f68a138063719451083de224bff64c
Alexander Kushnirenko <kushnirenko.alexander784@gmail.com>
2020-02-03 22:35:53 +0200

    new ConstEdgeIterator class declaration and implementation

---
+++ source/ConstEdgeIterator.cpp
@@ -0,0 +1,38 @@
+
+#include "ConstEdgeIterator.h"
+
+Graph::ConstEdgeIterator::ConstEdgeIterator(Graph* graph_, int vertexId_, bool isEnded):
+    graph(graph_), vertexId(vertexId_), ended(isEnded) { }
+
+auto Graph::ConstEdgeIterator::operator++() -> self_type {
+    incrementIterator();
+    return *this;
+}
+
+auto Graph::ConstEdgeIterator::operator++(int) -> self_type {
+    self_type temp = *this;
+    incrementIterator();
+    return temp;
+}
+
+auto Graph::ConstEdgeIterator::operator*() -> const value_type& {
+    if (ended) {
+        pointer ptr{nullptr};
+        return *ptr;
+    }
+    return graph->cont[vertexId].second[edgeNumber];
+}
+
+auto Graph::ConstEdgeIterator::operator->() -> pointer {
+    if (ended) {
+        pointer ptr{nullptr};
+        return &*ptr;
+    }
+    return &graph->cont[vertexId].second[edgeNumber];
+}
+
+void Graph::ConstEdgeIterator::incrementIterator() {
+    ++edgeNumber;
+    ended = edgeNumber >= graph->cont[vertexId].second.size();
+}
+
---
+++ source/ConstEdgeIterator.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <iterator>
+
+#include "Graph.h"
+#include "Edge.h"
+
+class Graph::ConstEdgeIterator
+{
+public:
+
+    using self_type = Graph::ConstEdgeIterator;
+    using value_type = Edge;
+    using reference = const Edge&;
+    using pointer = const Edge*;
+    using iterator_category = std::bidirectional_iterator_tag;
+    using difference = int;
+
+    ConstEdgeIterator(Graph*, int vertexId_, bool ended = false);
+
+    self_type operator++();
+    self_type operator++(int);
+
+    const value_type& operator*();
+    pointer operator->();
+
+    friend bool operator==(const self_type& ob1, const self_type& ob2) {
+        return (ob1.ended == ob2.ended && ((ob1.ended == true) ||
+                (ob1.graph == ob2.graph && ob1.vertexId == ob2.vertexId && ob1.edgeNumber == ob2.edgeNumber)));
+    }
+
+    friend bool operator!=(const self_type& ob1, const self_type& ob2) {
+        return !(ob1 == ob2);
+    }
+
+private:
+
+    void incrementIterator();
+
+    Graph* graph;
+    int vertexId{0};
+    int edgeNumber{0};
+    bool ended{false};
+};
+
